b31fedc565e98f8ed32e0a2cf5663c01b0b9a672
Chidi Orji <orjichidi95@gmail.com>
2020-06-13 00:05:28 +0100

    chore: setup store and test renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
 
-import App from './App';
+import { render, screen } from "./custom-render";
+
+import App from "./App";
 
 describe("<App />", () => {
   it("Renders <App /> component correctly", () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Getting started with React testing library/i)).toBeInTheDocument();
+    render(<App />);
+    expect(
+      screen.getByText(/Getting started with React testing library/i)
+    ).toBeInTheDocument();
   });
 });
