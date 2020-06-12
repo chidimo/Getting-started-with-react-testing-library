e636e3d3324dd660d37919178412bd360575660a
Chidi Orji <orjichidi95@gmail.com>
2020-06-12 19:24:33 +0100

    chore: setup test and style

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Getting started with React testing library/i)).toBeInTheDocument();
+  });
 });
